d842adc5a410305a41de8429a10e65e967e67f6e
Robinson Ibarra <ribarra764@gmail.com>
2025-01-30 14:17:47 -0800

    test: no-ref: commit for current place in practice

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,1 +1,20 @@
-console.log('typescript tutorial')
+const john: { id: number; name: string; isActive: boolean } = {
+  id: 1,
+  name: "john",
+  isActive: true,
+};
+const susan: { id: number; name: string; isActive: boolean } = {
+  id: 1,
+  name: "susan",
+  isActive: false,
+};
+
+function createUser(user: { id: number; name: string; isActive: boolean }): {
+  id: number;
+  name: string;
+  isActive: boolean;
+} {
+  console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+
+  return user;
+}
