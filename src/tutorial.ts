68aeca8af25b476a9937b74cf02e796df5546073
Robinson Ibarra <ribarra764@gmail.com>
2025-01-31 17:13:35 -0800

    challenge: no-ref: challenge completion with type aliases

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -32,3 +32,21 @@ function setTheme(t:Theme){
 
 setTheme('dark')
 
+type Employee = {id: number; name: string; dpt: string};
+type Manager = { id: number; name: string; employees: Employee[] };
+type Staff = Employee | Manager;
+
+function printStaffDetails(staff: Staff){
+    if ('employees' in staff){
+        console.log(`Employee is a Manager and their staff: ${staff.employees.length}`)
+    } else {
+        console.log(`Is an employee of department: ${staff.dpt}`)
+    }
+}
+let jeff: Staff = {id: 1, name: 'Jeff', dpt: 'Accounting'}
+let alice: Staff = { id: 1, name: 'Alice', dpt: "Accounting" };
+let bob: Staff = {id: 0, name: "Robert", employees: [jeff,alice]};
+
+printStaffDetails(jeff)
+printStaffDetails(bob)
+
