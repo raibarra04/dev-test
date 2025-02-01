4e499d3043e3b9805b6fa308180970b25ddbe44d
Robinson Ibarra <ribarra764@gmail.com>
2025-02-01 13:42:36 -0800

    challenge: no-ref: interface challenge complete

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,52 +1,21 @@
-export type User = { id: number; name: string; isActive: boolean };
-
-const john: User = {
-  id: 1,
-  name: "john",
-  isActive: true,
-};
-const susan: User = {
-  id: 1,
-  name: "susan",
-  isActive: false,
-};
-
-function createUser(user: User): User {
-  console.log(`Hello there ${user.name.toUpperCase()} !!!`);
-
-  return user;
+// Interface Challenge
+interface Computer {
+    readonly id: number;
+    brand: string;
+    ram: number;
+    storage?: number;
+    upgradeRam(inc:number): number;
 }
 
-type StringOrNumber = string | number;
-
-let value: StringOrNumber;
-
-type Theme = 'light' | 'dark';
-
-let theme: Theme;
-theme = 'dark'
-
-function setTheme(t:Theme){
-    theme = t;
-}
-
-setTheme('dark')
-
-type Employee = {id: number; name: string; dpt: string};
-type Manager = { id: number; name: string; employees: Employee[] };
-type Staff = Employee | Manager;
-
-function printStaffDetails(staff: Staff){
-    if ('employees' in staff){
-        console.log(`Employee is a Manager and their staff: ${staff.employees.length}`)
-    } else {
-        console.log(`Is an employee of department: ${staff.dpt}`)
+let myPC: Computer = {
+    id: 1,
+    brand: 'Dell',
+    ram: 2,
+    upgradeRam(inc){
+        return this.ram + inc;
     }
 }
-let jeff: Staff = {id: 1, name: 'Jeff', dpt: 'Accounting'}
-let alice: Staff = { id: 1, name: 'Alice', dpt: "Accounting" };
-let bob: Staff = {id: 0, name: "Robert", employees: [jeff,alice]};
 
-printStaffDetails(jeff)
-printStaffDetails(bob)
+const newRam = myPC.upgradeRam(10);
 
+console.log(newRam)
