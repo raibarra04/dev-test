3e428a245c5376e3029a6d02872cef2e83aae366
Robinson Ibarra <ribarra764@gmail.com>
2025-01-31 16:47:27 -0800

    fix: no-ref: adding type aliases

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,20 +1,34 @@
-const john: { id: number; name: string; isActive: boolean } = {
+export type User = { id: number; name: string; isActive: boolean };
+
+const john: User = {
   id: 1,
   name: "john",
   isActive: true,
 };
-const susan: { id: number; name: string; isActive: boolean } = {
+const susan: User = {
   id: 1,
   name: "susan",
   isActive: false,
 };
 
-function createUser(user: { id: number; name: string; isActive: boolean }): {
-  id: number;
-  name: string;
-  isActive: boolean;
-} {
+function createUser(user: User): User {
   console.log(`Hello there ${user.name.toUpperCase()} !!!`);
 
   return user;
 }
+
+type StringOrNumber = string | number;
+
+let value: StringOrNumber;
+
+type Theme = 'light' | 'dark';
+
+let theme: Theme;
+theme = 'dark'
+
+function setTheme(t:Theme){
+    theme = t;
+}
+
+setTheme('dark')
+
